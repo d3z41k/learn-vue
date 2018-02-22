4b73691068751d89e33bb5ee76b2d64837bb5249
Denn Zhokhoff <d3z41k@gmail.com>
2018-02-22 15:04:14 +0200

    Step 8: Components Within Components

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+Vue.component('task-list', {
+    template: 
+        `<div>
+            <task v-for="task in tasks">{{ task.task }}</task>
+        </div`,
+
+    data() {
+        return {
+            tasks: [
+                { task: 'Go to the store', completed: true},
+                { task: 'Finish screencast', completed: false},
+                { task: 'Make donation', completed: false},
+                { task: 'Clear inbox', completed: false},
+                { task: 'Make dinner', completed: false},
+                { task: 'Clean room', completed: true}
+            ]
+        }
+    }
+})
+
 Vue.component('task', {
     template: '<li><slot></slot></li>'
 })
