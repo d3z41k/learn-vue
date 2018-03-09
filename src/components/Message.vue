ba6f51004b89edaa43764f015f41025e07de8af2
d3z41k <d3z41k@gmail.com>
2018-03-09 14:39:42 +0200

    Step 16: Webpack and vue-cli

---
+++ src/components/Message.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="box">
+        <p>
+            <slot></slot>
+        </p>
+    </div> 
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+    .box { background: burlywood; padding: 10px; border: 1px solid black }
+</style>
+
