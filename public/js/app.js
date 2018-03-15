4e535625cd6140148051068ebd9ef2756f63e913
Denn Zhokhoff <d3z41k@gmail.com>
2018-03-15 09:21:56 +0200

    Step 19-21: Object-Oriented Forms

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,127 @@
-Vue.prototype.$http = axios
+class Errors {
+	constructor() {
+		this.errors = {}
+	}
 
-new Vue({
-    el: '#app',
+	has(field) {
+		return this.errors.hasOwnProperty(field)
+	}
 
-    data: {
-        skills: []
-    },
+	any() {
+		return Object.keys(this.errors).length > 0
+	}
 
-    mounted() {
-        this.$http.get('/skills').then(response => this.skills = response.data)
+	get(field) {
+		if (this.errors[field]) {
+			return this.errors[field][0]
+		}
+	}
+
+	record(errors) {
+		this.errors = errors
+	}
+
+	clear(field) {
+		if (field) {
+			delete this.errors[field]
+
+			return
+		}
+		
+		this.errors = {}
+	}
+}
+
+class Form {
+
+	constructor(data) {
+		this.originalData = data
+
+		for (let field in data) {
+			this[field] = data[field]
+		}
+
+		this.errors = new Errors()
+	}
+
+	data() {
+		let data = {}
+
+        for (let property in this.originalData) {
+            data[property] = this[property]
+        }
+
+		return data
+	}
+
+	reset() {
+		for (let field in this.originalData) {
+			this[field] = ''
+		}
+
+		this.errors.clear()
+	}
+
+	post(url) {
+        return this.submit('post', url)
     }
 
+    put(url) {
+        return this.submit('put', url)
+    }
+
+    patch(url) {
+        return this.submit('patch', url)
+    }
+
+    delete(url) {
+        return this.submit('delete', url)
+    }
+
+	submit(requestType, url) {
+
+		return new Promise((resolve, reject) => {
+			axios[requestType](url, this.data())
+				.then(response => {
+					this.onSuccess(response.data)
+
+					resolve(response.data)
+				})
+				.catch(error => {
+					this.onFail(error.response.data.errors)
+
+					reject(error.response.data.errors)
+				})
+		})
+
+	}
+
+	onSuccess(data) {
+		alert(data.message)
+
+		this.reset()
+	}
+
+	onFail(errors) {
+		this.errors.record(errors)
+	}
+
+}
+
+var app = new Vue({
+	el: '#app',
+
+	data: {
+		form: new Form({
+			name: '',
+			description: '',
+		})
+	},
+
+	methods: {
+		onSubmit() {
+			this.form.post('/projects')
+                .then(response => console.log('Complete!'))
+		}
+	}
 })
